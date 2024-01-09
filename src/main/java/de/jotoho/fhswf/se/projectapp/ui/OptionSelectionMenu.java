11f0abbd0b192ee5d9b6c3b4c0400994c84091db
Jonas Tobias Hopusch <git@jotoho.de>
2024-01-09 03:15:44 +0100

    Roll back all changes in OptionSelectionMenu to current master

--- src/main/java/de/jotoho/fhswf/se/projectapp/ui/OptionSelectionMenu.java
+++ src/main/java/de/jotoho/fhswf/se/projectapp/ui/OptionSelectionMenu.java
@@ -1,19 +1,22 @@
 package de.jotoho.fhswf.se.projectapp.ui;
 
-import java.util.*;
-import java.util.concurrent.Callable;
-
 import static java.util.Collections.unmodifiableSequencedSet;
 import static java.util.Objects.*;
 import static java.util.function.Predicate.not;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.concurrent.Callable;
+
 @SuppressWarnings("unused")
 public final class OptionSelectionMenu<T> {
     public record Option<T>(String label,
-                            Set<String> aliases,
-                            boolean enumerate,
-                            T value,
-                            Callable<T> code) {
+                         Set<String> aliases,
+                         boolean enumerate,
+                         T value,
+                         Callable<T> code) {
 
         public T get() {
             if (nonNull(value))
@@ -45,8 +48,8 @@ public final class OptionSelectionMenu<T> {
         this(prompt);
         this.options.addAll(
                 options.stream()
-                        .filter(Objects::nonNull)
-                        .toList()
+                       .filter(Objects::nonNull)
+                       .toList()
         );
     }
 
@@ -54,7 +57,7 @@ public final class OptionSelectionMenu<T> {
     public OptionSelectionMenu(final String prompt,
                                final Option<T>... options) {
         this(prompt,
-                Arrays.asList(options));
+             Arrays.asList(options));
     }
 
     public synchronized void addOption(final Option<T> option) {
@@ -66,7 +69,7 @@ public final class OptionSelectionMenu<T> {
         if (nonNull(option)) {
             this.options.remove(option);
             if (this.selectedOption.isPresent()
-                    && this.selectedOption.filter(option::equals).isPresent()) {
+                && this.selectedOption.filter(option::equals).isPresent()) {
                 this.selectedOption = Optional.empty();
             }
         }
@@ -90,7 +93,7 @@ public final class OptionSelectionMenu<T> {
     private void printOptionsAsList() {
         final List<Option<T>> enumeratedOptions = this.getEnumeratedOptions();
         final int colsNumber = Math.max(1,
-                (int) Math.ceil(Math.log10(enumeratedOptions.size() - 1)));
+                                        (int) Math.ceil(Math.log10(enumeratedOptions.size() - 1)));
         if (!enumeratedOptions.isEmpty()) {
             System.out.println("Nummerierte Möglichkeiten:");
         }
@@ -98,14 +101,14 @@ public final class OptionSelectionMenu<T> {
             final var opt = enumeratedOptions.get(index);
             final String indexStr =
                     "0".repeat(colsNumber - 1)
-                            .concat(Integer.valueOf(index)
-                                    .toString());
+                       .concat(Integer.valueOf(index)
+                                      .toString());
             System.out.println(indexStr + ". " + opt.label + " " + opt.aliases().toString());
         }
 
         final List<Option<T>> aliasOptions = this.options.stream()
-                .filter(not(enumeratedOptions::contains))
-                .toList();
+                                                         .filter(not(enumeratedOptions::contains))
+                                                         .toList();
         if (!aliasOptions.isEmpty()) {
             System.out.println("Benannte Möglichkeiten:");
             for (final var opt : aliasOptions) {
@@ -122,51 +125,51 @@ public final class OptionSelectionMenu<T> {
         boolean freshActivation = true;
         this.selectedOption = Optional.empty();
         if (this.options.stream()
-                .noneMatch(opt -> opt.enumerate || !opt.aliases().isEmpty())) {
+                        .noneMatch(opt -> opt.enumerate || !opt.aliases().isEmpty())) {
             return;
         }
 
         final var scanner = new Scanner(System.in);
 
-        while (isNull(this.selectedOption) || freshActivation ||
-                this.selectedOption.filter(this.options::contains).isEmpty()) {
-            if (freshActivation)
-                freshActivation = false;
-            try {
-                System.out.println();
-                System.out.println(this.prompt);
+            while (isNull(this.selectedOption) || freshActivation ||
+                   this.selectedOption.filter(this.options::contains).isEmpty()) {
+                if (freshActivation)
+                    freshActivation = false;
+                try {
+                    System.out.println();
+                    System.out.println(this.prompt);
 
-                if (this.listingEnabled)
-                    System.out.print("Wählen Sie eine Option (l um alle anzuzeigen): ");
-                else
-                    System.out.print("Wählen Sie eine Option: ");
+                    if (this.listingEnabled)
+                        System.out.print("Wählen Sie eine Option (l um alle anzuzeigen): ");
+                    else
+                        System.out.print("Wählen Sie eine Option: ");
 
-                final String userInput = scanner.nextLine().strip();
+                    final String userInput = scanner.nextLine().strip();
 
-                if (this.listingEnabled && userInput.equals("l")) {
-                    printOptionsAsList();
-                    continue;
+                    if (this.listingEnabled && userInput.equals("l")) {
+                        printOptionsAsList();
+                        continue;
+                    }
+
+                    final var aliasHits = this.options.stream()
+                                                      .filter(opt -> opt.aliases()
+                                                                        .contains(userInput))
+                                                      .toList();
+
+                    if (!aliasHits.isEmpty()) {
+                        this.selectedOption = Optional.of(aliasHits.getFirst());
+                        continue;
+                    }
+
+                    final int index = Integer.parseInt(userInput, 10);
+                    final var enumerators = this.getEnumeratedOptions();
+                    if (enumerators.size() >= index + 1) {
+                        this.selectedOption = Optional.of(enumerators.get(index));
+                    }
                 }
-
-                final var aliasHits = this.options.stream()
-                        .filter(opt -> opt.aliases()
-                                .contains(userInput))
-                        .toList();
-
-                if (!aliasHits.isEmpty()) {
-                    this.selectedOption = Optional.of(aliasHits.getFirst());
-                    continue;
-                }
-
-                final int index = Integer.parseInt(userInput, 10);
-                final var enumerators = this.getEnumeratedOptions();
-                if (enumerators.size() >= index + 1) {
-                    this.selectedOption = Optional.of(enumerators.get(index));
+                catch (final NumberFormatException ignore) {
                 }
             }
-            catch (final NumberFormatException ignore) {
-            }
-        }
 
     }
 
